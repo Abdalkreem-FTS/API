1d647c0ac0c999ec77e78b9ba9470c9218adf6a6
Abdalkreem Bzoor <abdalkreem.bzoor@foothillsolutions.com>
2026-08-09 11:10:09 +0300

    Add Stub for the TokenRevocationStore

--- API.Tests/TestApiFactory.cs
+++ API.Tests/TestApiFactory.cs
@@ -1,21 +1,14 @@
+using API.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace API.Tests;
 
 public sealed class TestApiFactory : WebApplicationFactory<Program>
 {
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
-        builder.UseSetting("ConnectionStrings:Redis", "localhost:6379");
+    private StubTokenRevocationStore RevocationStore { get; } = new();
 
-        builder.ConfigureServices(services =>
-        {
-            services.RemoveAll<IDistributedCache>();
-            services.AddDistributedMemoryCache();
-        });
-    }
+    protected override void ConfigureWebHost(IWebHostBuilder builder) =>
+        builder.ConfigureServices(services => services.AddSingleton<ITokenRevocationStore>(RevocationStore));
 }
